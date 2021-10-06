d9afb6d9e2a76dcd57f2dde8cee679851c3046cd
codeliner <kontakt@codeliner.ws>
2021-10-06 19:28:40 +0200

    Fix codyconfig.ts

--- codyconfig.ts
+++ codyconfig.ts
@@ -1,11 +1,8 @@
-import {onCommandHook} from "./src/hooks/onCommandHook";
-import {onAggregateHook} from "./src/hooks/onAggregateHook";
-
 const CodyConfig = {
   context: {},
   hooks: {
-    onCommand: onCommandHook,
-    onAggregate: onAggregateHook,
+//    onCommand: onCommandHook,
+//    onAggregate: onAggregateHook,
 //    onEvent: onEventHook,
 //    onPolicy: onPolicyHook,
 //    onDocument: onDocumentHook,
